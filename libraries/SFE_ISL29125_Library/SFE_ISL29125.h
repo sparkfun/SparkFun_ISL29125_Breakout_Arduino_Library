3cf26aab9f2043236e0842b14af6ce4e424920ac
Jordan McConnell <jordan.mcconnell@sparkfun.com>
2014-04-18 11:24:35 -0600

    Added configuration comments to library .h file

--- libraries/SFE_ISL29125_Library/SFE_ISL29125.h
+++ libraries/SFE_ISL29125_Library/SFE_ISL29125.h
@@ -26,26 +26,27 @@ Distributed as-is; no warranty is given.
 #define ISL_I2C_ADDR 0x44
 
 // ISL29125 Registers
-#define DEVICE_ID 0x00 // default id is 0x7d, write 0x46 in 'the write only' to reset all registers to default states
-#define CONFIG_1 0x01 // b2:b0 mode - 101 rgb, 100 standby, b3 range - 0 375 lux, 1 10,000 lux, b4 resolution - 0 16bits, 1 12 bits, b5 sync - 0 adc start at i2c write 0x01, 1 adc start at rising !int
-#define CONFIG_2 0x02 // b5:b0 active ir compensation, b7 ir offset, not sure what default should be, see graph on page 11 of datasheet
-#define CONFIG_3 0x03 // b1:b0 interrupt threshold - 00 no interrupt, 01 green, 10 red, 11 blue, b3:b2 interrupt persist control (# of interrupt conditions that must happen before interrupt flag and !int pin is set) - 00 1, 01 2, 10 4, 11 8, b4 rgb conversion done to !int control - 0 disable, 1 enable
-#define THRESHOLD_LL 0x04 // lower interrupt threshold (causes interrupt when measurement is below this value), 0 by default
+#define DEVICE_ID 0x00
+#define CONFIG_1 0x01
+#define CONFIG_2 0x02
+#define CONFIG_3 0x03
+#define THRESHOLD_LL 0x04
 #define THRESHOLD_LH 0x05
-#define THRESHOLD_HL 0x06 // high interrupt threshold (causes interrupt when measurement is above this value), 0 by default
+#define THRESHOLD_HL 0x06
 #define THRESHOLD_HH 0x07
-#define STATUS 0x08 // b0 RGBTHF - 0 interrupt is cleared or not triggered, 1 interrupt is triggered, b1 conversion - 0 still convert or cleared, 1 conversion completed, b2 brown out - 0 no brownout, 1 powerdown or brownout occured, b5:b4 rgb conversion - 00 no operation, 01 green, 10 red, 11 blue
-#define GREEN_L 0x09 // green adc value
+#define STATUS 0x08 
+#define GREEN_L 0x09 
 #define GREEN_H 0x0A
-#define RED_L 0x0B // red adc value
+#define RED_L 0x0B
 #define RED_H 0x0C
-#define BLUE_L 0x0D // blue adc value
+#define BLUE_L 0x0D
 #define BLUE_H 0x0E
 
 // Configuration Settings
 #define CFG_DEFAULT 0x00
 
 // CONFIG1
+// Pick a mode, determines what color[s] the sensor samples, if any
 #define CFG1_MODE_POWERDOWN 0x00
 #define CFG1_MODE_G 0x01
 #define CFG1_MODE_R 0x02
@@ -55,34 +56,47 @@ Distributed as-is; no warranty is given.
 #define CFG1_MODE_RG 0x06
 #define CFG1_MODE_GB 0x07
 
+// Light intensity range
+// In a dark environment 375Lux is best, otherwise 10KLux is likely the best option
 #define CFG1_375LUX 0x00
 #define CFG1_10KLUX 0x08
 
+// Change this to 12 bit if you want less accuracy, but faster sensor reads
+// At default 16 bit, each sensor sample for a given color is about ~100ms
 #define CFG1_16BIT 0x00
 #define CFG1_12BIT 0x10
 
+// Unless you want the interrupt pin to be an input that triggers sensor sampling, leave this on normal
 #define CFG1_ADC_SYNC_NORMAL 0x00
 #define CFG1_ADC_SYNC_TO_INT 0x20
 
 // CONFIG2
+// Selects upper or lower range of IR filtering
 #define CFG2_IR_OFFSET_OFF 0x00
 #define CFG2_IR_OFFSET_ON 0x80
 
+// Sets amount of IR filtering, can use these presets or any value between 0x00 and 0x3F
+// Consult datasheet for detailed IR filtering calibration
 #define CFG2_IR_ADJUST_LOW 0x00
 #define CFG2_IR_ADJUST_MID 0x20
 #define CFG2_IR_ADJUST_HIGH 0x3F
 
 // CONFIG3
+// No interrupts, or interrupts based on a selected color
 #define CFG3_NO_INT 0x00
 #define CFG3_G_INT 0x01
 #define CFG3_R_INT 0x02
 #define CFG3_B_INT 0x03
 
+// How many times a sensor sample must hit a threshold before triggering an interrupt
+// More consecutive samples means more times between interrupts, but less triggers from short transients
 #define CFG3_INT_PRST1 0x00
 #define CFG3_INT_PRST2 0x04
 #define CFG3_INT_PRST4 0x08
 #define CFG3_INT_PRST8 0x0C
 
+// If you would rather have interrupts trigger when a sensor sampling is complete, enable this
+// If this is disabled, interrupts are based on comparing sensor data to threshold settings
 #define CFG3_RGB_CONV_TO_INT_DISABLE 0x00
 #define CFG3_RGB_CONV_TO_INT_ENABLE 0x10
 
